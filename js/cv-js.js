86de51c77a09087a55e0b71e4088ad6df9846040
4xDMG <dajtait87@gmail.com>
2017-01-08 12:08:56 +1000

    Added ajax contact form and php mailer from tutorial.

--- js/cv-js.js
+++ js/cv-js.js
@@ -4,5 +4,43 @@ $(document).ready(function() {
 		$(".nav li").removeClass("active");
 		$(this).addClass("active");
 	});
+
+	$(function() {
+		var form = $('#ajax-contact');
+		var formMessages = $("#form-messages");
+
+		$(form).submit(function(event) {
+			event.preventDefault();
+			var formData = $(form).serialize();
+
+			$.ajax({
+				type: "POST",
+				url: $(form).attr("action"),
+				data: formData
+			})
+
+			.done(function(response) {
+				$(formMessages).removeClass('error');
+				$(formMessages).addClass('success');
+
+				$(formMessages).text(response);
+
+				$('#name').val('');
+				$('#email').val('');
+				$('#message').val('');
+			})
+
+			.fail(function(data) {
+				$(formMessages).removeClass('success');
+				$(formMessages.addClass('error'));
+
+				if (data.responseText !== '') {
+					$(formMessages).text(data.responseText);
+				} else {
+					$(formMessages).text("An error has occured and your message could not be sent.")
+				}
+			})
+		})
+	});
 	
 });
